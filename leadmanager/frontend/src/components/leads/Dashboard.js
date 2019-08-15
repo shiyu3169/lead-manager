ea1b990c8395c3cdc59af62805429e95f0eea63e
Shiyu Wang <shiyu3169@gmail.com>
2019-08-15 14:32:17 -0400

    main components

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import From, { Form } from './Form';
+import Leads from './Leads';
+
+function Dashboard() {
+  return (
+    <>
+      <Form />
+      <Leads />
+    </>
+  );
+}
+
+export default Dashboard;
