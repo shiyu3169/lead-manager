253e1b185704bbf9594b8397211c574e33ef36f5
Shiyu Wang <shiyu3169@gmail.com>
2019-08-20 12:29:04 -0400

    auth component and auth check

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Header from './layout/Header';
-import Dashboard from './leads/Dashboard';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import store from '../store';
+import { Provider } from 'react-redux';
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 import Alerts from './layout/Alerts';
-import { Provider } from 'react-redux';
-import store from '../store';
-
+import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
+import { loadUser } from '../actions/auth';
 // Alert Options
 const alertOptions = {
   timeout: 3000,
@@ -15,15 +24,25 @@ const alertOptions = {
 };
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Header />
-          <Alerts />
-          <div className='container'>
-            <Dashboard />
-          </div>
+          <Router>
+            <Header />
+            <Alerts />
+            <div className='container'>
+              <Switch>
+                <PrivateRoute exact path='/' component={Dashboard} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Router>
         </AlertProvider>
       </Provider>
     );
